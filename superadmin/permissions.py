ac9144cb17c73e225ed5ed204f87161f13654b81
Henry Iheonu <henryiheonu264@gmail.com>
2025-10-24 11:34:29 +0100

    chore: ignore __pycache__ and *.pyc

--- superadmin/permissions.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsSuperAdmin(BasePermission):
-    def has_permission(self, request, view):
-        u = request.user
-        return bool(u and u.is_authenticated and u.is_superuser)
