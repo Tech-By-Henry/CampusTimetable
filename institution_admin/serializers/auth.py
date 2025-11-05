d9d43861468f37cbf3a169b7f5d2aaff46023eda
Henry Iheonu <henryiheonu264@gmail.com>
2025-11-05 09:36:33 +0100

    pulled and installed dependencies (and also funded)

--- institution_admin/serializers/auth.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import serializers
-
-class AdminLoginSerializer(serializers.Serializer):
-    staff_id = serializers.CharField(max_length=64)
-    password = serializers.CharField(write_only=True, min_length=8)
